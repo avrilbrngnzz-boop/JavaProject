1c928e9bc1d45be8cf8d4b1fb9c3c70f8740b34f
Ludivine <ludivine.goirand69@gmail.com>
2026-04-17 13:56:17 +0200

    phase de test

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javaproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javaproject to javafx.fxml;
-    exports com.example.javaproject;
-}
