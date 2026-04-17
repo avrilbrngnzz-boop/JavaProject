c01327df569a1f6e1264accd632b97e12ddd3a43
Ludivine <ludivine.goirand69@gmail.com>
2026-04-17 15:52:46 +0200

    debut de faire le controller du quiz

--- src/main/java/com/example/javaproject/QuizController.java
+++ src/main/java/com/example/javaproject/QuizController.java
@@ -1,4 +1,40 @@
 package com.example.javaproject;
 
+import com.google.gson.Gson;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.net.HttpCookie;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class QuizController {
+    @FXML private Label labelQuestion, labelScore, labelFeedback;
+    @FXML private VBox containerReponses;
+
+    private int score = 0;
+    private String reponseCorrecte;
+    private Gson gson = new Gson();
+
+    @FXML
+    public void initialize() {
+        chargerNouvelleQuestion();
+    }
+
+    private void chargerNouvelleQuestion(){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://opentdb.com/api_config.php?amount=1&type=multiple"))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(this::traiterReponseAPI)
+                .exceptionally(ex -> {
+                    ex.printStackTrace();
+                    return null;
+                });
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.javaproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
+    requires java.net.http;
 
     opens com.example.javaproject to javafx.fxml, com.google.gson;
     exports com.example.javaproject;
